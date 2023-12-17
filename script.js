8d23d7917eb5fbec95f3268442ed4883d28422f7
Aman_Kant <amankant565@gmail.com>
2023-12-17 17:12:19 +0530

    files added

---
+++ script.js
@@ -0,0 +1,36 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '448bca70c1msh2c3463cf872e715p1c6a9ajsn8359bdbff475',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+const getWeather = (city)=>{
+	cityName.innerHTML = city
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+	.then(response => response.json())
+	.then(response => {
+		console.log(response)
+		cloud_pct.innerHTML = response.cloud_pct
+		feels_like.innerHTML = response.feels_like
+		humidity.innerHTML = response.humidity
+		humidity2.innerHTML = response.humidity
+		max_temp.innerHTML = response.max_temp
+		min_temp.innerHTML = response.min_temp
+		sunrise.innerHTML = response.sunrise
+		sunset.innerHTML = response.sunset
+		temp.innerHTML = response.temp
+		temp2.innerHTML = response.temp
+		wind_speed.innerHTML = response.wind_speed
+		wind_speed2.innerHTML = response.wind_speed
+
+	})
+	.catch(err => console.error(err));
+}
+
+submit.addEventListener("click", (e)=>{
+	e.preventDefault()
+	getWeather(city.value)
+})
+
+getWeather("Delhi")
